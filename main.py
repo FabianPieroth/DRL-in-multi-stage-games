381ed9de97e73bbd41c27545ca186fe9eb910e94
ge69hoj <fabian.pieroth@tum.de>
2022-04-06 15:55:09 +0200

    Restructure main call for readability and using hydra

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+from typing import Dict
+
 import hydra
 import torch
 
 from src.envs.rock_paper_scissors import RockPaperScissors
+from src.envs.sequential_auction import SequentialAuction
+from src.envs.torch_vec_env import BaseEnvForVec
 
 
 def get_config():
@@ -10,11 +14,21 @@ def get_config():
     return cfg
 
 
+def get_env(config: Dict) -> BaseEnvForVec:
+    env_name = config["rl_envs"]["name"]
+    if env_name == "rockpaperscissors":
+        return RockPaperScissors(config["rl_envs"], device=config["device"])
+    elif env_name == "sequential_auction":
+        return SequentialAuction(config["rl_envs"], device=config["device"])
+    else:
+        raise ValueError("No known env chosen, check again: " + str(env_name))
+
+
 def main():
     config = get_config()
-    env = RockPaperScissors(config, device=1)
+    env = get_env(config)
     starting_states = env.sample_new_states(123)
-    actions = torch.randint(low=0, high=27, size=(123, 1))
+    actions = torch.randint(low=0, high=3, size=(123, 3))
     info = env.compute_step(starting_states, actions)
 
 
