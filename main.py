9ff8386506f6406288e513d7cba6cee29b7a6514
ge69hoj <fabian.pieroth@tum.de>
2022-05-19 08:57:03 +0200

    Enable running multiple experiments by adding loop in main

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import copy
+
 import src.utils_folder.env_utils as env_ut
 import src.utils_folder.io_utils as io_ut
 from src.learners.multi_agent_learner import MultiAgentCoordinator
@@ -5,16 +7,17 @@ from src.learners.multi_agent_learner import MultiAgentCoordinator
 
 def main():
     config = io_ut.get_and_store_config()
-    env = env_ut.get_env(config)
-    ma_learner = MultiAgentCoordinator(config, env)
-    # train the agents
-    ma_learner.learn(
-        total_timesteps=config["total_training_steps"],
-        log_interval=1,
-        eval_freq=config["eval_freq"],
-        n_eval_episodes=5,
-        tb_log_name="MultiAgent",
-    )
+    for i in range(1):  # You can change adapted config inside the loop
+        adapted_config = copy.deepcopy(config)
+        env = env_ut.get_env(adapted_config)
+        ma_learner = MultiAgentCoordinator(adapted_config, env)
+        ma_learner.learn(
+            total_timesteps=adapted_config["total_training_steps"],
+            log_interval=1,
+            eval_freq=adapted_config["eval_freq"],
+            n_eval_episodes=5,
+            tb_log_name="MultiAgent",
+        )
 
 
 if __name__ == "__main__":
