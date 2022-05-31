8f01beb6adf764b1d4e44aa0e7afbf478c69e208
ge69hoj <fabian.pieroth@tum.de>
2022-05-31 09:06:49 +0200

    Increase test coverage to different algo settings

---
+++ tests/test_algorithms.py
@@ -0,0 +1,29 @@
+"""This module tests a single iteration for each combination of algorithms."""
+import hydra
+import pytest
+
+import src.utils_folder.io_utils as io_ut
+import src.utils_folder.test_utils as tst_ut
+
+ids, testdata = zip(
+    *[
+        ["all_ppo", ["ppo", "ppo", "ppo"]],
+        ["ppo_ppo_dummy", ["ppo", "ppo", "rps_single_action"]],
+        ["dummy_ppo_dummy", ["rps_single_action", "ppo", "rps_single_action"]],
+        ["all_dummy", ["rps_single_action", "rps_single_action", "rps_single_action"]],
+    ]
+)
+
+
+@pytest.mark.parametrize("algorithms", testdata, ids=ids)
+def test_algos_in_rockpaperscissors(algorithms):
+    config = io_ut.get_and_store_config()
+    config["iteration_num"] = 1
+    io_ut.enrich_config(config)
+    print(config["total_training_steps"])
+    rl_envs = hydra.compose("../configs/rl_envs/rockpaperscissors.yaml")[""][""][""][
+        "configs"
+    ]["rl_envs"]
+    config["rl_envs"] = rl_envs
+    config["algorithms"] = algorithms
+    tst_ut.run_limited_learning(config)
