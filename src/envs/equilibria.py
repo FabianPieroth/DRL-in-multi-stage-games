a81cd11b85a12883a50419ac5d15b146dae8037d
ge69hoj <fabian.pieroth@tum.de>
2022-06-02 18:16:38 +0200

    Add no-signaling second stage equilibrium strategy

--- src/envs/equilibria.py
+++ src/envs/equilibria.py
@@ -1,4 +1,6 @@
 """Collection of known equilibria."""
+from multiprocessing.sharedctypes import Value
+
 import torch
 
 
@@ -30,6 +32,51 @@ def truthful(num_agents: int, num_units: int, player_position: int = 0):
 
         if won is not None:
             bid[won, ...] = 0
+
+        return bid.view(-1, 1)
+
+    return bid_function
+
+
+def no_signaling_equilibrium(num_agents: int, prior_low: float, prior_high: float):
+    """Equilibrium strategy for two stage signaling contest. First round all-pay,
+    second round tullock contest. True valuations of winner revealed."""
+
+    def bid_function(
+        round: int,
+        valuations: torch.Tensor,
+        signal_info: torch.Tensor,
+        lost: torch.Tensor = None,
+    ):
+        if round == 1:
+            bid = valuations
+        elif round == 2:
+            bid = (valuations ** 2 * signal_info) / (valuations + signal_info) ** 2
+        else:
+            raise ValueError("Only two stage contest implemented!")
+
+        if lost is not None:
+            bid[lost, ...] = 0
+
+        return bid.view(-1, 1)
+
+    return bid_function
+
+
+def signaling_equilibrium(num_agents: int, prior_low: float, prior_high: float):
+    """Equilibrium strategy for two stage signaling contest. First round all-pay,
+    second round tullock contest. Bid of winner revealed."""
+
+    def bid_function(
+        round: int,
+        valuation: torch.Tensor,
+        signal_info: torch.Tensor,
+        lost: torch.Tensor = None,
+    ):
+        bid = ((num_agents - num_units) / (num_agents - stage - 1)) * valuation
+
+        if lost is not None:
+            bid[lost, ...] = 0
 
         return bid.view(-1, 1)
 
