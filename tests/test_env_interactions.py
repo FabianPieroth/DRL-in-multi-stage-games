9dfa5e00710c6877257ad2b3e53857913fca6c92
ge69hoj <fabian.pieroth@tum.de>
2022-06-02 15:55:08 +0200

    Add env interaction test for signaling contest

--- tests/test_env_interactions.py
+++ tests/test_env_interactions.py
@@ -23,7 +23,7 @@ def test_learning_rockpaperscissors():
     tst_ut.run_limited_learning(config)
 
 
-ids, testdata = zip(
+ids_sequ_auction, testdata_sequ_auction = zip(
     *[
         ["first-price", ("first", 2, True, False)],
         ["second-price", ("second", 2, True, False)],
@@ -36,15 +36,14 @@ ids, testdata = zip(
 
 @pytest.mark.parametrize(
     "mechanism_type,num_agents,reduced_observation_space,collapse_symmetric_opponents",
-    testdata,
-    ids=ids,
+    testdata_sequ_auction,
+    ids=ids_sequ_auction,
 )
 def test_learning_sequential_auctions(
     mechanism_type, num_agents, reduced_observation_space, collapse_symmetric_opponents
 ):
     """Runs multi agent learning in sequential auctions for specified parameters."""
     config = io_ut.get_and_store_config()
-    config["total_training_steps"] = 1
     config["policy_sharing"] = True
 
     rl_envs = hydra.compose("../configs/rl_envs/sequential_auction.yaml")[""][""][""][
@@ -57,5 +56,39 @@ def test_learning_sequential_auctions(
     config["rl_envs"]["num_rounds_to_play"] = num_agents - 1
     config["rl_envs"]["reduced_observation_space"] = reduced_observation_space
     config["rl_envs"]["collapse_symmetric_opponents"] = collapse_symmetric_opponents
+
+    io_ut.enrich_config(config)
+    config["total_training_steps"] = 1
+
+    tst_ut.run_limited_learning(config)
+
+
+ids_sign_contest, testdata_sign_contest = zip(
+    *[
+        ["no-signaling-small", ("true_valuations", 4)],
+        ["no-signaling-large", ("true_valuations", 4)],
+        ["signaling-small", ("winning_bids", 8)],
+        ["signaling-large", ("winning_bids", 8)],
+    ]
+)
+
+
+@pytest.mark.parametrize(
+    "information_case, num_agents", testdata_sign_contest, ids=ids_sign_contest
+)
+def test_learning_signaling_contest(information_case, num_agents):
+    """Runs multi agent learning in sequential auctions for specified parameters."""
+    config = io_ut.get_and_store_config()
+    config["policy_sharing"] = True
+    config["algorithms"] = ["ppo" for _ in range(num_agents)]
+    rl_envs = hydra.compose("../configs/rl_envs/signaling_contest.yaml")[""][""][""][
+        "configs"
+    ]["rl_envs"]
+    rl_envs["information_case"] = information_case
+    config["rl_envs"] = rl_envs
+    config["rl_envs"]["num_agents"] = num_agents
+
+    io_ut.enrich_config(config)
+    config["total_training_steps"] = 1
 
     tst_ut.run_limited_learning(config)
