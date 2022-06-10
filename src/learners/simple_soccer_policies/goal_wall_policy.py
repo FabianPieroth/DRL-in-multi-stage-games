f1ae3a406184d01dc5c723383e99918887c352f5
ge69hoj <fabian.pieroth@tum.de>
2022-06-10 18:29:17 +0200

    Add goal wall policy

--- src/learners/simple_soccer_policies/goal_wall_policy.py
+++ src/learners/simple_soccer_policies/goal_wall_policy.py
@@ -1,16 +1,51 @@
 import math
+from typing import Any, Dict, Optional, Tuple, Type, Union
 
+import gym
 import torch
-from simplesoccer.mini_env_states import SoccerStates
-from simplesoccer.policies.common import EvalPolicy
-from simplesoccer.simple_soccer import SimpleSoccer
+from stable_baselines3.common.policies import BasePolicy
+from stable_baselines3.common.type_aliases import GymEnv, Schedule
+
+from src.envs.simple_soccer import SimpleSoccer
+from src.learners.base_learner import MABaseAlgorithm
+from src.learners.simple_soccer_policies.common import extract_data_from_obs
 
 
-class GoalWallPolicy(EvalPolicy):
-    def __init__(self, device, n_players, visualize=False):
-        dummy_env = SimpleSoccer(device, opponent_policy=None)
+class GoalWallPolicy(MABaseAlgorithm):
+    def __init__(
+        self,
+        agent_id: int,
+        config: Dict,
+        policy: Type[BasePolicy] = None,
+        env: Union[GymEnv, str, None] = None,
+        policy_base: Type[BasePolicy] = ...,
+        learning_rate: Union[float, Schedule] = 0,
+        policy_kwargs: Optional[Dict[str, Any]] = None,
+        tensorboard_log: Optional[str] = None,
+        verbose: int = 0,
+        device: Union[torch.device, str] = "auto",
+        support_multi_env: bool = True,
+        create_eval_env: bool = False,
+        monitor_wrapper: bool = True,
+        seed: Optional[int] = None,
+        use_sde: bool = False,
+        sde_sample_freq: int = -1,
+        supported_action_spaces: Optional[Tuple[gym.spaces.Space, ...]] = None,
+    ):
+        super().__init__(
+            agent_id,
+            config,
+            policy,
+            env,
+            device=device,
+            tensorboard_log=tensorboard_log,
+        )
+        self.full_config = config
+        self.config = config["algorithm_configs"]["soccer_chase_ball"]
+        dummy_env = SimpleSoccer(self.full_config["rl_envs"], device)
 
-        self.device = device
+        self.n_players = dummy_env._n_players_per_team
+        self.agent_team_id = self.agent_id % self.n_players
 
         self.pi = torch.tensor(math.pi, dtype=torch.float32, device=self.device)
         self.our_goal_pos = torch.tensor(
@@ -18,28 +53,41 @@ class GoalWallPolicy(EvalPolicy):
         )
         self.goal_halfwidth = dummy_env.goal_width / 2
         left = -self.goal_halfwidth + dummy_env.player_radius
-        target_x = torch.linspace(left, -left, steps=n_players, device=device)
+        target_x = torch.linspace(left, -left, steps=self.n_players, device=device)
         target_y = -dummy_env.halffield_height + 1.5
-        target_y = torch.full(size=(n_players,), fill_value=target_y, device=device)
+        target_y = torch.full(
+            size=(self.n_players,), fill_value=target_y, device=device
+        )
         self.target_pos = torch.column_stack((target_x, target_y))
 
-        self.visualize = visualize
+        self.visualize = False
         self.viz_elems = []
 
-        self.n_players = n_players
+    def ingest_data_to_learner(
+        self,
+        sa_actions,
+        sa_rewards,
+        sa_additional_actions_data,
+        dones,
+        infos,
+        new_obs,
+        agent_id: int,
+        policy_sharing: bool,
+        callback,
+    ):
+        pass
 
-    def compute_actions(self, states: SoccerStates):
-        """
-        :param states: Vectorized states
-        :return: Vectorized motion_actions
-        """
-        assert len(states.batch_shape) == 1
-        n_players = self.n_players
-        assert states.objects.shape[1] == n_players * 2 + 1
-
-        ball_pos = states.objects[..., 0, :, 0]  # [B, XY]
-        ours_pos = states.objects[..., 1 : 1 + n_players, :, 0]  # [B, K, XY]
-
+    def predict(
+        self,
+        observation: torch.Tensor,
+        state: Optional[Tuple[torch.Tensor, ...]] = None,
+        episode_start: Optional[torch.Tensor] = None,
+        deterministic: bool = False,
+    ) -> Tuple[torch.Tensor, Optional[Tuple[torch.Tensor, ...]]]:
+        ball_pos, ball_vel, player_pos, player_vel, player_ener = extract_data_from_obs(
+            observation, self.n_players * 2
+        )
+        ours_pos = player_pos[:, : self.n_players, :]
         ball_delta_pos = ball_pos[..., None, :] - ours_pos
 
         # Get ranks of players according to their x position, such that
@@ -62,12 +110,35 @@ class GoalWallPolicy(EvalPolicy):
         motion_actions = discrete_angle_to_action[discrete_angle]
 
         kick_actions = ball_delta_pos[..., :, 1] > 0
+
         action_template = torch.zeros(
-            states.batch_shape + (n_players * 3,), dtype=torch.int64, device=self.device
+            (observation.shape[0], self.n_players * 3),
+            dtype=torch.int64,
+            device=self.device,
         )
 
         action_template[..., ::3] = motion_actions
         action_template[..., 1::3] = 1  # everyone dashes
         action_template[..., 2::3] = 2 * kick_actions
 
-        return action_template
+        return (
+            action_template[:, self.agent_team_id * 3 : (self.agent_team_id + 1) * 3],
+            state,
+        )
+
+    def get_actions_with_data(
+        self, agent_id: int
+    ) -> Tuple[torch.Tensor, torch.Tensor, Tuple]:
+        """Computes actions for the current state for env.step()
+
+        Args:
+            agent_id (int): determines which agents_actions will be returned
+
+        Returns:
+            actions_for_env: possibly adapted actions for env
+            actions: predicted actions by policy
+            additional_actions_data: additional data needed for algorithm later on
+        """
+        agent_obs = self._last_obs[agent_id]
+        agent_actions, _ = self.predict(agent_obs)
+        return agent_actions, agent_actions, ()
