22f848bef354f3525cbe9ff4de69dd40da29bbe6
ge69hoj <fabian.pieroth@tum.de>
2022-10-28 11:35:07 +0200

    Increase readability by comments and expressive methods

--- src/verifier/brute_force_verifier.py
+++ src/verifier/brute_force_verifier.py
@@ -114,88 +114,162 @@ class BFVerifier(BaseVerifier):
         batch_utilities = torch.zeros(
             self._total_utilities_shape_for_batch(batch_size), device=self.device
         ).flatten()
-        cur_sim_size = batch_size
+        sim_size = batch_size  # The current size of the simulation
 
         states = self.env.model.sample_new_states(batch_size)
+
         agent_obs, opp_obs = self._split_obs(
             agent_id, self.env.model.get_observations(states)
         )
         episode_starts = torch.ones((batch_size,), dtype=bool, device=self.device)
-        batch_indices = torch.tensor([], device=self.device).long()
+        sim_batch_indices = torch.tensor([], device=self.device).long()
 
         for stage in range(self.num_rounds_to_play):
-            # TODO: Write a method with corresponding test for the repeation here. It looks correct atm.
-            obs_bin_indices = self.env.model.get_obs_bin_indices(
-                agent_obs, agent_id, stage, self.obs_discretization
+            sim_size_states = self._get_sim_size_states(states)
+            sim_size_obs_bins = self._get_sim_size_obs_bins(agent_id, agent_obs, stage)
+            sim_size_action_bins = self._get_sim_size_action_bins(sim_size, stage)
+            sim_batch_indices = torch.cat(
+                (sim_batch_indices, sim_size_obs_bins, sim_size_action_bins), dim=-1
             )
-            repeated_obs_bin_indices = self._repeat_and_flatten_to_full_sim_size(
-                obs_bin_indices, stage
-            ).unsqueeze(-1)
-            action_bins = torch.arange(self.action_discretization, device=self.device)
-            repeated_action_bins = (
-                self._repeat_tensor_along_new_axis(
-                    data=action_bins,
-                    pos=[0, 2],
-                    repeats=[
-                        cur_sim_size,
-                        self.action_discretization
-                        ** (self.num_rounds_to_play - stage - 1),
-                    ],
-                )
-                .flatten()
-                .unsqueeze(-1)
-            )
-            batch_indices = torch.cat(
-                (batch_indices, repeated_obs_bin_indices, repeated_action_bins), dim=-1
-            )
-            repeated_grid_actions = self._get_agent_grid_actions(agent_id, cur_sim_size)
+            sim_size_grid_actions = self._get_agent_grid_actions(agent_id, sim_size)
 
-            cur_sim_size *= self.action_discretization
-
-            opp_actions = self._get_opponent_actions(
-                episode_starts, learners, agent_id, opp_obs
-            )
-            for opp_agent, opp_action in opp_actions.items():
-                opp_actions[opp_agent] = self._repeat_tensor_along_new_axis(
-                    data=opp_action, pos=[1], repeats=[self.action_discretization]
-                )
-
-            repeated_states = self._repeat_tensor_along_new_axis(
-                data=states, pos=[1], repeats=[self.action_discretization]
+            opp_actions = self._get_sim_size_opp_actions(
+                learners, agent_id, opp_obs, episode_starts
             )
 
-            combined_actions = {}
-            for agent_identifier in range(self.num_agents):
-                if agent_identifier == agent_id:
-                    combined_actions[agent_identifier] = repeated_grid_actions.flatten(
-                        end_dim=1
-                    )
-                else:
-                    combined_actions[agent_identifier] = opp_actions[
-                        agent_identifier
-                    ].flatten(end_dim=1)
-            flattened_states = repeated_states.flatten(end_dim=1)
+            combined_actions = self._get_combined_actions(
+                agent_id, sim_size_grid_actions, opp_actions
+            )
 
+            flattened_states = sim_size_states.flatten(end_dim=1)
             new_obs, rewards, dones, new_states = self.env.model.compute_step(
                 flattened_states, combined_actions
             )
-            repeated_agent_rewards = self._repeat_and_flatten_to_full_sim_size(
+            repeated_agent_rewards = self._repeat_and_flatten_to_full_stage_sim_size(
                 rewards[agent_id], stage + 1
             )
             batch_utilities += repeated_agent_rewards
-            episode_starts = dones
 
+            episode_starts = dones
             agent_obs, opp_obs = self._split_obs(agent_id, new_obs)
             states = new_states
+            sim_size *= self.action_discretization
 
         assert (
             torch.all(dones).cpu().item()
-        ), "The game should have ended after playing all rounds! Check num_rounds_to_play of env!"
-        information_tree.add_simulation_results(batch_utilities, batch_indices)
+        ), "All games should have ended after playing all rounds! Check num_rounds_to_play of env!"
+        information_tree.add_simulation_results(batch_utilities, sim_batch_indices)
 
-    def _repeat_and_flatten_to_full_sim_size(
+    def _get_combined_actions(self, agent_id, sim_size_grid_actions, opp_actions):
+        combined_actions = {}
+        for agent_identifier in range(self.num_agents):
+            if agent_identifier == agent_id:
+                combined_actions[agent_identifier] = sim_size_grid_actions.flatten(
+                    end_dim=1
+                )
+            else:
+                combined_actions[agent_identifier] = opp_actions[
+                    agent_identifier
+                ].flatten(end_dim=1)
+
+        return combined_actions
+
+    def _get_sim_size_opp_actions(
+        self,
+        learners,
+        agent_id: int,
+        opp_obs: torch.Tensor,
+        episode_starts: torch.Tensor,
+    ) -> Dict[int, torch.Tensor]:
+        """Get opponent actions Dict[agent_id: (sim_size, action_size)] and repeat
+        for any possible action taken by agent agent_id:
+            Dict[agent_id: (sim_size, action_discretization, action_size)]
+        Returns:
+            Dict[int, torch.Tensor]:
+        """
+        opp_actions = self._get_opponent_actions(
+            episode_starts, learners, agent_id, opp_obs
+        )
+        for opp_agent, opp_action in opp_actions.items():
+            opp_actions[opp_agent] = self._repeat_tensor_along_new_axis(
+                data=opp_action, pos=[1], repeats=[self.action_discretization]
+            )
+        return opp_actions
+
+    def _get_sim_size_states(self, states: torch.Tensor) -> torch.Tensor:
+        """Repeat states for each possible action taken.
+        Args:
+            states (torch.Tensor): shape: (sim_size, )
+
+        Returns:
+            torch.Tensor: shape: (sim_size, action_discretization)
+        """
+        sim_size_states = self._repeat_tensor_along_new_axis(
+            data=states, pos=[1], repeats=[self.action_discretization]
+        )
+
+        return sim_size_states
+
+    def _get_sim_size_action_bins(self, sim_size: int, stage: int) -> torch.Tensor:
+        """Get repeated action bin indices to track tree path indices.
+        Args:
+            sim_size (int): 
+            stage (int): 
+
+        Returns:
+            torch.Tensor: 
+        """
+        action_bins = torch.arange(self.action_discretization, device=self.device)
+        repeated_action_bins = (
+            self._repeat_tensor_along_new_axis(
+                data=action_bins,
+                pos=[0, 2],
+                repeats=[
+                    sim_size,
+                    self.action_discretization ** (self.num_rounds_to_play - stage - 1),
+                ],
+            )
+            .flatten()
+            .unsqueeze(-1)
+        )
+
+        return repeated_action_bins
+
+    def _get_sim_size_obs_bins(
+        self, agent_id: int, agent_obs: torch.Tensor, stage: int
+    ) -> torch.Tensor:
+        """Indices of bins for each simulation of current stage. Repeat this, as
+        this information is needed for all branching simulations to index the 
+        final path followed by each simulation.
+
+        Args:
+            agent_id (int):
+            agent_obs (torch.Tensor): 
+            stage (int):
+
+        Returns:
+            torch.Tensor: (sim_size * sims_to_be_made, 1)
+        """
+        obs_bin_indices = self.env.model.get_obs_bin_indices(
+            agent_obs, agent_id, stage, self.obs_discretization
+        )
+        repeated_obs_bin_indices = self._repeat_and_flatten_to_full_stage_sim_size(
+            obs_bin_indices, stage
+        ).unsqueeze(-1)
+
+        return repeated_obs_bin_indices
+
+    def _repeat_and_flatten_to_full_stage_sim_size(
         self, data: torch.Tensor, stage: int
     ) -> torch.Tensor:
+        """Repeats a tensor to be the full size of the simulation.
+        Args:
+            data (torch.Tensor): _description_
+            stage (int): _description_
+
+        Returns:
+            torch.Tensor: shape=(sim_size * sims_to_be_made)
+        """
         pos_to_repeat = len(data.shape)
         return self._repeat_tensor_along_new_axis(
             data=data,
