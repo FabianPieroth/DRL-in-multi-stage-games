dc9506e56429553e292b49126f1edd3a629bb955
ge69hoj <fabian.pieroth@tum.de>
2022-11-23 11:00:08 +0100

    Delete redundant equ method for SequentialAuction

--- src/envs/equilibria.py
+++ src/envs/equilibria.py
@@ -148,40 +148,6 @@ class SequetialAuctionEquilibrium(EquilibriumStrategy):
         return bid_function
 
 
-def equilibrium_fpsb_symmetric_uniform(
-    num_agents: int, num_units: int, player_position: int = 0
-):
-    """Equilibrium for FPSB symmetric uniform prior sequential auction."""
-
-    assert (
-        num_agents > num_units
-    ), "For this BNE, there must be more bidders than items."
-
-    def bid_function(stage: int, valuation: torch.Tensor, won: torch.Tensor = None):
-        bid = ((num_agents - num_units) / (num_agents - stage)) * valuation
-
-        if won is not None:
-            bid[won, ...] = 0
-
-        return bid.view(-1, 1)
-
-    return bid_function
-
-
-def truthful(num_agents: int, num_units: int, player_position: int = 0):
-    """Truthful bidding."""
-
-    def bid_function(stage: int, valuation: torch.Tensor, won: torch.Tensor = None):
-        bid = ((num_agents - num_units) / (num_agents - stage - 1)) * valuation
-
-        if won is not None:
-            bid[won, ...] = 0
-
-        return bid.view(-1, 1)
-
-    return bid_function
-
-
 def winning_effect_term(valuations: torch.Tensor) -> torch.Tensor:
     term_1 = 27.0 * torch.log(valuations + 1.5) - 17.0 / 2.0 * valuations
     term_2 = -43.0 / 4.0 * math.log(2.5)
