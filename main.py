843124e730cf87e57dbb45fa60c3aca9066df0ba
ge69hoj <fabian.pieroth@tum.de>
2022-12-08 11:21:41 +0100

    Delete arguments from ma_learner.learn() to improve front-end clarity

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-import copy
-
-import hydra
-
 import src.utils.env_utils as env_ut
 import src.utils.io_utils as io_ut
 from src.learners.multi_agent_learner import MultiAgentCoordinator
@@ -9,20 +5,14 @@ from src.learners.multi_agent_learner import MultiAgentCoordinator
 
 def main():
 
-    for i in range(1):  # You can change adapted config inside the loop
+    for i in range(1):  # You can change adapted config with overrides inside the loop
 
         config = io_ut.get_config()
 
         # run learning
         env = env_ut.get_env(config)
         ma_learner = MultiAgentCoordinator(config, env)
-        ma_learner.learn(
-            total_timesteps=config.total_training_steps,
-            n_steps_per_iteration=config.n_steps_per_iteration,
-            log_interval=1,
-            eval_freq=config.eval_freq,
-            n_eval_episodes=5,
-        )
+        ma_learner.learn()
 
         io_ut.wrap_up_experiment_logging(config)
 
