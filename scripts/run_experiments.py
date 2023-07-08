bbf863f62f6bacdeef7dbbed19916f5e7fe408ab
Fabian Pieroth <fabian.pieroth@tum.de>
2023-07-08 10:24:53 +0200

    Resolve "Unify experiments and add pretify option for plots for WINE conference"

--- scripts/run_experiments.py
+++ scripts/run_experiments.py
@@ -103,8 +103,8 @@ def run_asymmetric_second_price_sequential_sales_experiment():
     print("Done!")
 
 
-def run_asymmetric_budget_constraint_experiments():
-    environment = "asymmetric_budget_constraint_experiments"
+def run_symmetric_budget_constraint_with_affiliation_experiments():
+    environment = "symmetric_budget_constraint_plus_affiliations"
     log_path = f"{LOG_PATH}/{environment}_experiment/"
 
     device = 1
@@ -114,51 +114,8 @@ def run_asymmetric_budget_constraint_experiments():
     num_rounds_to_play = 2
     mechanism_type_options = ["first", "second"]
     algorithm_options = ["reinforce", "ppo"]
-    options = product(mechanism_type_options, algorithm_options)
-    budgets = [0.15, 0.2, 0.25]
-
-    for option in options:
-        mechanism_type, algorithm = option
-        for i in range(runs):
-            print("=============\nStart new run\n-------------")
-
-            # Configure and set hyperparameters
-            config = io_ut.get_config(
-                overrides=[
-                    f"device={device}",
-                    f"seed={i}",
-                    f"algorithms=[{algorithm}]",
-                    f"iteration_num={iteration_num}",
-                    f"eval_freq={iteration_num}",
-                    f"algorithm_configs.{algorithm}.n_rollout_steps={num_rounds_to_play}",
-                    f"policy_sharing={False}",
-                    f"log_path={log_path}",
-                    f"verify_br={True}",
-                    f"rl_envs.mechanism_type={mechanism_type}",
-                    f"rl_envs.num_rounds_to_play={num_rounds_to_play}",
-                    f"rl_envs.num_agents={num_rounds_to_play + 1}",
-                    f"rl_envs.budgets={budgets}",
-                ]
-            )
-
-            # Set up env and learning
-            coord_ut.start_ma_learning(config)
-
-    print("Done!")
-
-
-def run_symmetric_budget_constraint_experiments():
-    environment = "symmetric_budget_constraint_experiments"
-    log_path = f"{LOG_PATH}/{environment}_experiment/"
-
-    device = 1
-    runs = 10
-    iteration_num = 10_000
-
-    num_rounds_to_play = 2
-    mechanism_type_options = ["first", "second"]
-    algorithm_options = ["reinforce", "ppo"]
-    budgets_options = [0.15, 0.2, 0.25]
+    budgets_options = [0.6, 0.8]
+    sampler = "affiliated_uniform"
     options = product(mechanism_type_options, algorithm_options, budgets_options)
 
     for option in options:
@@ -182,57 +139,7 @@ def run_symmetric_budget_constraint_experiments():
                     f"rl_envs.num_rounds_to_play={num_rounds_to_play}",
                     f"rl_envs.num_agents={num_rounds_to_play + 1}",
                     f"rl_envs.budgets={budgets}",
-                ]
-            )
-
-            # Set up env and learning
-            coord_ut.start_ma_learning(config)
-
-    print("Done!")
-
-
-def run_sequential_sales_risk_experiment():
-    environment = "sequential_auction"
-    log_path = f"{LOG_PATH}/{environment}_risk_experiment/"
-
-    device = 1
-    runs = 10
-    iteration_num = 10_000
-    policy_sharing = True
-
-    num_rounds_to_play_options = [2]
-    mechanism_type_options = ["first", "second"]
-    algorithm_options = ["ppo", "reinforce"]
-    risk_aversion_options = [0.25, 0.5, 0.75, 1.0]
-    options = product(
-        num_rounds_to_play_options,
-        mechanism_type_options,
-        algorithm_options,
-        risk_aversion_options,
-    )
-
-    for option in options:
-        num_rounds_to_play, mechanism_type, algorithm, risk_aversion = option
-
-        for i in range(runs):
-            print("=============\nStart new run\n-------------")
-
-            # Configure and set hyperparameters
-            config = io_ut.get_config(
-                overrides=[
-                    f"device={device}",
-                    f"seed={i}",
-                    f"algorithms=[{algorithm}]",
-                    f"iteration_num={iteration_num}",
-                    f"eval_freq={iteration_num}",
-                    f"algorithm_configs.{algorithm}.n_rollout_steps={num_rounds_to_play}",
-                    f"policy_sharing={policy_sharing}",
-                    f"log_path={log_path}",
-                    f"verify_br={True}",
-                    f"rl_envs.mechanism_type={mechanism_type}",
-                    f"rl_envs.num_rounds_to_play={num_rounds_to_play}",
-                    f"rl_envs.num_agents={num_rounds_to_play + 1}",
-                    f"rl_envs.risk_aversion={risk_aversion}",
+                    f"rl_envs/sampler={sampler}",
                 ]
             )
 
@@ -252,7 +159,7 @@ def run_sequential_sales_interdependent_plus_risk_experiment():
     policy_sharing = True
 
     num_rounds_to_play_options = [2]
-    algorithm_options = ["ppo", "reinforce"]
+    algorithm_options = ["reinforce", "ppo"]
     risk_aversion_options = [0.25, 0.5, 0.75]
 
     settings = [
@@ -291,60 +198,6 @@ def run_sequential_sales_interdependent_plus_risk_experiment():
                     f"rl_envs.num_agents={setting['num_agents'] + (num_rounds_to_play - 1)}",
                     f"rl_envs/sampler={setting['sampler_name']}",
                     f"rl_envs.risk_aversion={risk_aversion}",
-                ]
-            )
-
-            # Set up env and learning
-            coord_ut.start_ma_learning(config)
-
-    print("Done!")
-
-
-def run_sequential_sales_interdependent_experiment():
-    environment = "sequential_auction"
-    log_path = f"{LOG_PATH}/{environment}_interdependent_experiment/"
-
-    device = 1
-    runs = 10
-    iteration_num = 10_000
-    policy_sharing = True
-
-    num_rounds_to_play_options = [1, 2]
-    algorithm_options = ["ppo", "reinforce"]
-
-    settings = [
-        dict(
-            sampler_name="mineral_rights_common_value",
-            mechanism_type="second",
-            num_agents=3,
-        ),
-        dict(sampler_name="affiliated_uniform", mechanism_type="first", num_agents=2),
-    ]
-
-    options = product(num_rounds_to_play_options, algorithm_options, settings)
-
-    for option in options:
-        num_rounds_to_play, algorithm, setting = option
-
-        for i in range(runs):
-            print("=============\nStart new run\n-------------")
-
-            # Configure and set hyperparameters
-            config = io_ut.get_config(
-                overrides=[
-                    f"device={device}",
-                    f"seed={i}",
-                    f"algorithms=[{algorithm}]",
-                    f"iteration_num={iteration_num}",
-                    f"eval_freq={iteration_num}",
-                    f"algorithm_configs.{algorithm}.n_rollout_steps={num_rounds_to_play}",
-                    f"policy_sharing={policy_sharing}",
-                    f"log_path={log_path}",
-                    f"verify_br={True}",
-                    f"rl_envs.mechanism_type={setting['mechanism_type']}",
-                    f"rl_envs.num_rounds_to_play={num_rounds_to_play}",
-                    f"rl_envs.num_agents={setting['num_agents'] + (num_rounds_to_play - 1)}",
-                    f"rl_envs/sampler={setting['sampler_name']}",
                 ]
             )
 
@@ -402,9 +255,6 @@ def run_signaling_contest_experiment():
 if __name__ == "__main__":
     run_sequential_sales_experiment()
     run_asymmetric_second_price_sequential_sales_experiment()
-    run_asymmetric_budget_constraint_experiments()
-    run_symmetric_budget_constraint_experiments()
-    run_sequential_sales_risk_experiment()
-    run_sequential_sales_interdependent_experiment()
+    run_symmetric_budget_constraint_with_affiliation_experiments()
     run_sequential_sales_interdependent_plus_risk_experiment()
     run_signaling_contest_experiment()
