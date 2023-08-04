8c767a28d8d27b0c2c0de95d6a7c09344a931a1c
ge69hoj <fabian.pieroth@tum.de>
2023-08-04 12:32:44 +0200

    Delete redundant overrides

--- main.py
+++ main.py
@@ -4,12 +4,7 @@ import src.utils.io_utils as io_ut
 
 def main():
     for i in range(1):  # You can change adapted config with overrides inside the loop
-        overrides = [
-            f"rl_envs=coin_game",
-            f"num_envs=164",
-            f"eval_freq={1}",
-            f"device={6}",
-        ]
+        overrides = []
         config = io_ut.get_config(overrides=overrides)
         # run learning
         coord_ut.start_ma_learning(config)
