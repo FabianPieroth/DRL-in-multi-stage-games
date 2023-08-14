6d2c318325750bdd3cc55632c5f91b32437f4d95
Nils <nils.kohring@in.tum.de>
2023-08-14 09:43:18 +0200

    added sphinx config

---
+++ docs/source/conf.py
@@ -0,0 +1,37 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "Cont-MARL"
+copyright = "2023, FP, NK"
+author = "FP, NK"
+release = "0.0.1"
+
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath("../.."))  # Source code dir relative to this file
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+
+extensions = [
+    "sphinx.ext.autodoc",  # Core library for html generation from docstrings
+    "sphinx.ext.autosummary",  # Create neat summary tables
+]
+autosummary_generate = True  # Turn on sphinx.ext.autosummary
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "furo"
+html_static_path = ["_static"]
---
+++ docs/source/index.rst
@@ -0,0 +1,20 @@
+.. Cont-MARL documentation master file, created by
+   sphinx-quickstart on Mon Aug 14 09:35:42 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Cont-MARL's documentation!
+=====================================
+
+.. autosummary::
+   :toctree: _autosummary
+   :recursive:
+
+   src
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
